72ba326b5776e7a08332278f5e1197a2bb3ba734
StepanTsymbal <tsymbalstepan@gmail.com>
2024-07-20 19:36:01 +0300

    Update conftest.py

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-# from dotenv import load_dotenv
 import pytest
 from sqlalchemy.orm import sessionmaker, declarative_base
 import sqlalchemy as db
@@ -9,17 +8,15 @@ import app.utils as utils
 
 def db_prep():
     utils.init_creds()
-    # Base = declarative_base()
+
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
-    # DB_NAME = os.getenv('DB_NAME')
     DB_NAME = f"{os.getenv('DB_NAME')}_test"
     DB_HOST = os.getenv('DB_HOST')
 
     SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
     engine = db.create_engine(SQLALCHEMY_DATABASE_URL)
-    # Base.metadata.create_all(engine, tables=[Customers.__table__])
 
     return engine
 
